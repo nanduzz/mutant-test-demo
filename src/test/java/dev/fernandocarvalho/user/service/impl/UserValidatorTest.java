e33b4f7d17c10cf389fbfc7bfee2da72eccf5321
Fernando Carvalho <fernanduzz@gmail.com>
2022-07-27 21:08:02 -0300

    messing with tests for demo purpose

--- src/test/java/dev/fernandocarvalho/user/service/impl/UserValidatorTest.java
+++ src/test/java/dev/fernandocarvalho/user/service/impl/UserValidatorTest.java
@@ -1,6 +1,7 @@
 package dev.fernandocarvalho.user.service.impl;
 
 import dev.fernandocarvalho.user.models.User;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +23,15 @@ class UserValidatorTest {
                 RuntimeException.class,
                 () -> validator.validate(new User("", 33, true))
         );
+    }
+
+    @Test
+    void deveValidarUsuarioCorretamente(){
+        try {
+            validator.validate(new User("Teste", 33, true));
+        }catch (RuntimeException e){
+            Assertions.fail();
+        }
     }
 
 }
